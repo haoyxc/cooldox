427d5b0360c5510d941d0dc61ccfdaa55b9bbda2
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-05 12:00:32 -0400

    bolding functionality

--- client/src/components/Draft.js
+++ client/src/components/Draft.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, RichUtils } from "draft-js";
 
 function Draft() {
   const [editorState, setEditorState] = React.useState(EditorState.createEmpty());
-
+  const _onBoldClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
   return (
-    <div className="test">
+    <div className="draft-container">
       <p>editor</p>
+      <button onClick={this._onBoldClick.bind(this)}>Bold</button>
       <Editor editorState={editorState} onChange={setEditorState} />
     </div>
   );
