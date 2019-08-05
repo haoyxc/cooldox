9f91d5ec9a2b9b169987e63eaf68130579112713
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-05 12:57:59 -0400

    client side

--- client/src/components/Draft.js
+++ client/src/components/Draft.js
@@ -3,8 +3,12 @@ import { Editor, EditorState, RichUtils } from "draft-js";
 
 function Draft() {
   const [editorState, setEditorState] = React.useState(EditorState.createEmpty());
-  const _onBoldClick = () => {
+  const onBoldClick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+
+  const onItalicClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
   };
   return (
     <div className="draft-container">
