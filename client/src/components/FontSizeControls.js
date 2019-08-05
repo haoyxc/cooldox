e161bbdde029098fe3809c3f8456f0318c468b20
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-05 16:31:23 -0400

    added containers for portal and editing

--- client/src/components/FontSizeControls.js
+++ client/src/components/FontSizeControls.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import InlineButton from './InlineButton';
-import fontSizes from './Containers/fontSizes'
+import React, { useState } from "react";
+import InlineButton from "./InlineButton";
+import fontSizes from "./Containers/fontSizes";
 
 function FontSizeControls(props) {
-    const currentStyle = props.editorState.getCurrentInlineStyle();
+  const currentStyle = props.editorState.getCurrentInlineStyle();
 
-    return (
+  return (
     <div>
-        {fontSizes.map((type) =>
-              <InlineButton
-                key={type.label}
-                active={currentStyle.has(type.style)}
-                label={type.label}
-                onToggle={props.onToggle}
-                style={type.style}
-              />)}
+      {fontSizes.map(type => (
+        <InlineButton
+          className="fontsize-option"
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      ))}
     </div>
-    );
+  );
 }
 
 export default FontSizeControls;
