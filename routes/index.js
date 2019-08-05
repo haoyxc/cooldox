08a523f2f9b3b8a5504364f40d5bd33cacd4f9f6
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-05 17:07:35 -0400

    connect client to server in portal

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const models = require("../models");
+const User = models.User;
+const Document = models.Document;
 
 // all routes after login
+router.get("/portals", (req, res) => {
+  let user = req.user;
+});
 
 module.exports = router;
