a10a6e74fb79a0729388620fa131e906cfed4f8a
uchenma <uchenm@seas.upenn.edu>
2019-08-05 11:54:47 -0400

    server set up and inital passport set up

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/User");
+
+module.exports = function (passport, hash) {
+    return router
+}
