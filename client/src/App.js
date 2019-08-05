b91bb1b9c057f4bee3c488add780f9ffc2dbf288
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-05 11:50:16 -0400

    reconfigured draft files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Draft from "./components/Draft";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">HELLO!</header>
+      <Draft />
     </div>
   );
 }
