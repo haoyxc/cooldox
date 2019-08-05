c1286420854a8c5abea899d08d8b6f70e3fde33f
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-05 10:45:12 -0400

    initial commit

---
+++ client/public/electron.js
@@ -0,0 +1,6 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const isDev = require("electron-is-dev");
